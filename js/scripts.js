b887fc816f69192f82c335119c92b9588c06e140
Student <example@example.com>
2018-08-10 15:50:56 -0700

    Add all questions with successful find track.

--- js/scripts.js
+++ js/scripts.js
@@ -1,116 +1,36 @@
 $(document).ready(function() {
-        // get references to select list and display text box
 
     $("button#take").click(function() {
       $("form").show();
     });
 
     var sel = document.getElementById('scripts');
-    var el = document.getElementById('display');
-
     var sel_2 = document.getElementById('scripts-2');
-    var el_2 = document.getElementById('display-2');
-
     var sel_3 = document.getElementById('scripts-3');
-    var el_3 = document.getElementById('display-3');
 
+    // alert(userInput);
+  $("#trackForm").submit(function(event) {
 
-    function getSelectedOption(sel) {
-        var opt;
-        for ( var i = 0, len = sel.options.length; i < len; i++ ) {
-            opt = sel.options[i];
-            if ( opt.selected === true ) {
-                break;
-            }
-        }
-        return opt;
-    }
+    var sel = document.getElementById('scripts').value;
+    var sel_2 = document.getElementById('scripts-2').value;
+    var sel_3 = document.getElementById('scripts-3').value;
+    var sel_4 = document.getElementById('scripts-4').value;
+    var sel_5 = document.getElementById('scripts-5').value;
 
-    // assign onclick handlers to the buttons
-    document.getElementById('showVal').onclick = function () {
-        el.value = sel.value;
-    }
-    document.getElementById('showVal-2').onclick = function () {
-        el_2.value = sel_2.value;
-    }
-    document.getElementById('showVal-3').onclick = function () {
-        el_3.value = sel_3.value;
-    }
-
-
-
-  // alert(userInput);
-  $("#demoForm").submit(function(event) {
-
-    var nel = el.value = sel.value;
-    var nel_2 = el_2.value = sel_2.value;
-    var nel_3 = el_3.value = sel_3.value;
-
-      var userInput = nel + nel_2 + nel_3;
+    console.log(sel);
+    console.log(sel_2);
+    console.log(sel_3);
+    var userInput = sel + sel_2 + sel_3 + sel_4 + sel_5;
+      // alert(userInput);
 
     event.preventDefault();
-    if (userInput === PFU) {
+    if (userInput === "PFUsb" || userInput === "SFUsb") {
       $('#css_track').show();
-  // } else if (userInput === PBI) {
-  //         $('#ruby_track').show();
-  //       } else  (userInput === PBD) {
-  //         $('#csharp_track').show();
+    } else if (userInput === "PBIsb") {
+          $('#ruby_track').show();
+        } else if (userInput === "PBDsb") {
+          $('#csharp_track').show();
     }
+
   });
 });
-// immediate function to preserve global namespace
-
-
-
-
-
-//
-// var SF = function(S, F)) {
-//   return S + F;
-// }
-//
-// var SB = function(S, B)) {
-//   return S + B;
-// }
-//
-// var PF = function(P, F)) {
-//   return P + F;
-// }
-//
-// var PB = function(P, B)) {
-//   return P + B;
-// }
-//
-// var SF = 1
-// var SB = 11
-// var PF = 11
-// var PB = 21
-//
-//
-//
-//
-// $(document).ready(function() {
-//   $("form#track_test").submit(function(event) {
-//     event.preventDefault();
-//
-//     var  = $("value#S").val());
-//     var P = $("value#P").val());
-//
-//     var F = $("values#F").val());
-//     var B = $("values#B").val());
-//
-//     var userInput = text
-//
-//
-//
-//
-//     // if(userInput > 10) {
-//     //   $('#css_track').show();
-//     // } else if (userInput < 20 && height > 10) {
-//     //   $('#ruby_track').show();
-//     // } else  (userInput < 20) {
-//     //   $('#csharp_track').show();
-//     // }
-//
-//   });
-// });
