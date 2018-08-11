e2451ed15b1c1d3bc00ce944ab1d4cf883749316
Student <example@example.com>
2018-08-10 17:23:24 -0700

    Clean up code text.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,16 @@
 $(document).ready(function() {
 
-    $("button#take").click(function() {
-      $("form").show();
-    });
+  $("button#take").click(function() {
+    $("form").show();
+  });
 
-    // var sel = document.getElementById('scripts');
-    // var sel_2 = document.getElementById('scripts-2');
-    // var sel_3 = document.getElementById('scripts-3');
-
-    // alert(userInput);
   $("#trackForm").submit(function(event) {
-
     var sel = document.getElementById('scripts').value;
     var sel_2 = document.getElementById('scripts-2').value;
     var sel_3 = document.getElementById('scripts-3').value;
     var sel_4 = document.getElementById('scripts-4').value;
     var sel_5 = document.getElementById('scripts-5').value;
-
-    console.log(sel);
-    console.log(sel_2);
-    console.log(sel_3);
     var userInput = sel + sel_2 + sel_3 + sel_4 + sel_5;
-      // alert(userInput);
 
     event.preventDefault();
     if (userInput === "PFIsb" || userInput === "PFUsb" || userInput === "PFUss" || userInput === "PFUbb" || userInput === "SFUsb" || userInput === "SFUss" || userInput === "SFUbb") {
@@ -37,6 +26,5 @@ $(document).ready(function() {
       $('#ruby_track').hide();
       $('#csharp_track').show();
     }
-
   });
 });
