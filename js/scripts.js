51eb78e9109a05f74cf3fb045eb95878c7febe12
Student <example@example.com>
2018-08-17 11:20:59 -0700

    simplify logic structure with jQuery (see top part of js).

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,20 @@ $(document).ready(function() {
   });
 
   $("#trackForm").submit(function(event) {
-    var answer = document.getElementById('question').value;
-    var answer_2 = document.getElementById('question-2').value;
-    var answer_3 = document.getElementById('question-3').value;
-    var answer_4 = document.getElementById('question-4').value;
-    var answer_5 = document.getElementById('question-5').value;
-    var userInput = answer + answer_2 + answer_3 + answer_4 + answer_5;
-
     event.preventDefault();
+    var inputQuestions = ['#question', '#question-2', '#question-3', '#question-4', '#question-5'];
+    var userInput = [];
+    for (var i = 0; i < inputQuestions.length; i++) {
+      inputQuestion = $(inputQuestions[i]).val();
+      userInput.push(inputQuestion)};
+    userInput = userInput.join("");
+
     if (userInput === "PFIsb" || userInput === "PFUsb" || userInput === "PFUss" || userInput === "PFUbb" || userInput === "SFUsb" || userInput === "SFUss" || userInput === "SFUbb") {
       $('#ruby_track').hide();
       $('#csharp_track').hide();
       $('#css_track').show();
-    } else if (userInput === "PBIsb" || userInput === "PBIss" || userInput === "PBIbb" || userInput === "PFIss" || userInput === "PFIbb" || userInput === "PFIbr" || userInput === "PFUbr" || userInput === "SBIsb" || userInput === "SBIss" || userInput === "SBIbb" || userInput === "SFIsb" || userInput === "SFIss" || userInput === "SFIbb" || userInput === "SFUbr") {
+    } else if (userInput === "PBIsb" || userInput === "PBIss" || userInput === "PBIbb" || userInput === "PFIss" || userInput === "PFIbb" || userInput === "PFIbr" || userInput === "PFUbr" || userInput === "SBIsb" || userInput === "SBIss" || userInput === "SBIbb" || userInput === "SFIsb" || userInput === "SFIss" || userInput === "SFIbb" || userInput === "SFUbr")
+    {
       $('#css_track').hide();
       $('#csharp_track').hide();
       $('#ruby_track').show();
@@ -25,6 +26,6 @@ $(document).ready(function() {
       $('#css_track').hide();
       $('#ruby_track').hide();
       $('#csharp_track').show();
-    }
-  });
-});
+    } //close else if statement
+  }); //close "submit" button
+}); //close (document).ready
